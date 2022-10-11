60ca93b98b260881f7f97cc02a48431bf1338dec
kuppambhavya <112753722+kuppambhavya@users.noreply.github.com>
2022-10-11 13:43:45 +0530

    Update factors D1.cpp

--- factors D1.cpp
+++ factors D1.cpp
@@ -1,17 +1,35 @@
-#include<stdio.h>
-class number{	
-int num, i; 
-printf("Enter a positive integer: "); 
-scanf("%d", &num); 
-printf("Factors of %d are: ", num); 
-for (i = 1; i <= num; ++i) { 
-if (num % i == 0) { 
-printf("%d ", i); 
-} 
-}
-};
-int main() 
-{ 
-number num(); 
-return 0;
+#include <iostream>
+using namespace std;
+int main() {
+	int n, i;
+	cout<<"given number:";
+	cin >> n;
+	if (n <= 0) {
+		cout << "Invalid";
+		return 0;
+	}
+	int count = 0;
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			count++;
+		}
+	}
+	cout << "Number of factors = " << count << endl;
+	int N;
+	cout<<"N=";
+	cin >> N;
+	if (N > count) {
+		cout << "Invalid";
+		return 0;
+	}
+	int j = 1, k = 1;
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			if (j == N) {
+				cout << N << "th factor of " << n << " = " << i;
+				break;
+			}
+			j++;
+		}
+	}
 }
